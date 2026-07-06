7dd45f704758fa22d6c6a23b782d685e9a16fbe7
Shenss-uu <1658432912@qq.com>
2026-07-06 14:43:06 +0800

    修改累计学习时长算法

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -8,10 +8,16 @@ import './styles/theme.css'
 import './styles/animation.css'
 import App from './App.vue'
 import router from './router'
+import { startHeartbeat } from './utils/heartbeat'
 
 const app = createApp(App)
 Object.entries(ElementPlusIconsVue).forEach(([key, component]) => app.component(key, component))
 app.use(createPinia())
 app.use(router)
 app.use(ElementPlus)
+
+if (localStorage.getItem('access_token')) {
+  startHeartbeat()
+}
+
 app.mount('#app')
